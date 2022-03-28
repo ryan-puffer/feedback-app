dae18e45886309330db4c84623ec3ad41cce56c7
Ryan Puffer <ryanpuffer@Ryans-MacBook-Pro.local>
2022-03-27 20:58:22 -0500

    add FeedbackItem component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import Header from './components/Header';
+import FeedbackItem from './components/FeedbackItem';
 
 function App() {
-    return (
-        <>
-        <Header />
-        <div className='container'>
-            <h1>My App</h1>
-        </div>
-        </>
-    )
-    
+  return (
+    <>
+      <Header />
+      <FeedbackItem />
+      <div className="container">
+        <h1>Feedback App</h1>
+      </div>
+    </>
+  );
 }
 
 export default App;
